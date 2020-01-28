099140b0ccc891c3b18581086fdf2c3ed7f6014c
Cas Janse (DESKTOP) <cas.janse97@gmail.com>
2020-01-28 15:25:58 +0100

    Initial commit

---
+++ Assets/Scripts/TestingGrid.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGrid : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Grid grid = new Grid(20, 10);
+    }
+}
